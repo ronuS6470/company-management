dd66ac60c24ee0e8e5638daca862b98716c71add
Mitul Patel <mitul.patel@1rivet.local>
2020-02-28 12:27:29 +0530

    initial commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, SidebarComponent, ConfirmationModalComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
