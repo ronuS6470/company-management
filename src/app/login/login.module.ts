cce672a8ac4c571ba5c96fa13c52d5ee263efeaa
Aastha Yadav <aastha.yadav@1rivet.local>
2020-03-03 17:23:33 +0530

    Added

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginContainer } from './login-container/login.container';
 import { LoginPresentation } from './login-container/login-presentation/login.presentation';
-import { Login } from './login.model';
 
 
 @NgModule({
@@ -15,8 +14,7 @@ import { Login } from './login.model';
   imports: [
     CommonModule,
     LoginRoutingModule
-  ],
-  exports:
-  [Login]
+  ]
+  
 })
 export class LoginModule { }
