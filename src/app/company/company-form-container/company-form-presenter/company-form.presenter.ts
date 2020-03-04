afe360d864858157942f0ed438685782e65e59db
Kiran Tandel <kiran.tandel@1rivet.local>
2020-03-04 10:06:12 +0530

    feat:added file

--- src/app/company/company-form-container/company-form-presenter/company-form.presenter.ts
+++ src/app/company/company-form-container/company-form-presenter/company-form.presenter.ts
@@ -16,12 +16,12 @@ export class CompanyFormPresenter {
      */
     public buildCompanyForm(): FormGroup {
         return this.companyForm = this.formBuilder.group({
-            clientName: ['', [Validators.required, Validators.pattern("[a-zA-Z ]*")]],
+            clientName: ['', [Validators.required,Validators.pattern("[a-zA-Z]+(?:(?:\. |[' ])[a-zA-Z]+)*")]],
             clientType: ['supplier', Validators.required],
             businessType: ['', Validators.required],
             contactNumber: ['', [Validators.required, Validators.pattern('[0-9]{10,12}')]],
             location: ['', Validators.required],
-            email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9.+-_]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}')]],
+            email: ['', [Validators.required, Validators.pattern('[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}')]],
             contactPersonName: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
             designation: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
             contactPersonMobile: ['', [Validators.required, Validators.pattern('[0-9]{10,12}')]],
